b8d7ad9070b7b6776fb99568bda07f73e318e4c4
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-14 10:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	// shortHex = shortHex.replace(/^#/, '');   //use regex
+	shortHex = shortHex.replace("#", '');
+
+	if(shortHex.length === 3){
+		shortHex = shortHex.split('').map(e => e.repeat(2)).join('');
+	}  
+	return shortHex;
 };
 
 // Do not change the code below.
